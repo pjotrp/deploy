2c95d618538a73dda66b504a1a61c27b81de894d
pjotrp <pjotr.public01@thebird.nl>
2015-10-16 18:01:48 +0200

    Allow for globbing

--- lib/deploy/command.rb
+++ lib/deploy/command.rb
@@ -7,6 +7,7 @@ module Deploy
     # This is the exec function for one module (YAML file). Note that
     # it acts in a closure. So the last dir is shared in file.
     def Command.exec(fn,rundir,state)
+
       print "Run "+fn+"\n"
       list = YAML.load(File.read(fn))
       p list
@@ -15,48 +16,84 @@ module Deploy
       if not File.directory?(masterfiles)
         masterfiles = rundir + '/masterfiles'
       end
+
+      mkdir = lambda { |item,opts|
+        p [item,opts]
+        mode = 0755 # default
+        mode = opts['mode'].to_i(8) if opts and opts['mode']
+        # if it is not absolute, make it relative to $HOME
+        dest = item
+        if dest[0] != '/'
+          dest = state.homedir + '/' + item
+        end
+        p ['mkdir?',dest]
+        if not File.directory?(dest)
+          p ["mkdir",dest,mode.to_s(8)]
+          Dir.mkdir(dest,mode)
+        else
+          FileOps.chmod(dest,mode)
+        end
+        dest
+      }
+
+      copy_file = lambda { |item,opts| 
+        p [item,opts]
+        mode = 0644 # default
+        mode = opts['mode'].to_i(8) if opts and opts['mode']
+        dest =
+          if opts and opts['dest']
+            opts['dest']
+          else
+            destdir
+          end
+        # destination can be a file or directory
+        # if it is not absolute, make it relative to $HOME
+        if dest[0] != "/"
+          dest = state.homedir + '/' + dest
+        end
+        p [:destx,dest]
+        p [:itemx,item]
+        # source is a masterfile
+        source = item
+        if not File.exist?(item)
+          source = masterfiles+'/'+item
+        end
+        newfn = FileOps.copy_file(source,dest)
+        FileOps.chmod(newfn,mode)
+      }
+      
       list.each do | commands |
         commands.each do | command, items |
           p command
           case command 
           when 'dir' then
             items.each do |item,opts|
-              p [item,opts]
-              mode = 0755 # default
-              mode = opts['mode'].to_i(8) if opts and opts['mode']
-              # if it is not absolute, make it relative to $HOME
-              dest = item
-              if dest[0] != '/'
-                dest = state.homedir + '/' + item
-              end
-              if not File.directory?(dest)
-                p ["mkdir",dest,mode.to_s(8)]
-                Dir.mkdir(dest,mode)
-              else
-                FileOps.chmod(dest,mode)
-              end
-              destdir = dest
+              destdir = mkdir.call(item,opts)
             end
           when 'file' then
             items.each do |item,opts|
-              p [item,opts]
-              mode = 0644 # default
-              mode = opts['mode'].to_i(8) if opts and opts['mode']
-              dest =
-                if opts and opts['dest']
-                  opts['dest']
-                else
-                  destdir
-                end
-              # destination can be a file or directory
-              # if it is not absolute, make it relative to $HOME
-              if dest[0] != '/'
-                dest = state.homedir + '/' + dest
+              copy_file.call(item,opts)
+            end
+          when 'files' then
+            items.each do |glob,opts|
+              p [glob,opts]
+              source = masterfiles
+              if opts and opts['source']
+                source = rundir + '/' + opts['source']
               end
-              # source is a masterfile
-              source = masterfiles+'/'+item
-              newfn = FileOps.copy_file(source,dest)
-              FileOps.chmod(newfn,mode)
+              globbing = source + '/' + glob
+              p [:globbing,globbing]
+              Dir.glob(globbing) do |item|
+                p ['item',item]
+                dest = item[source.size+1..-1]
+                dest = destdir + '/' + dest
+                p ['dest',dest]
+                if File.directory?(item)
+                  destdir = mkdir.call(dest,opts)
+                else
+                  copy_file.call(item,opts)
+                end
+              end
             end
           else
             raise "UNKNOWN COMMAND "+command
